2b5196dbd824b106cc1bb04f66cc42ebf13d1918
WavyPeng <w1a9v9y3@gmail.com>
2018-07-02 13:49:30 +0800

    循环队列

---
+++ src/wavy/util/dsa/LoopQueue.java
@@ -0,0 +1,32 @@
+package wavy.util.dsa;
+
+/**
+ * 循环队列
+ * Created by WavyPeng on 2018/7/2.
+ */
+public class LoopQueue<E> implements Queue<E>{
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
