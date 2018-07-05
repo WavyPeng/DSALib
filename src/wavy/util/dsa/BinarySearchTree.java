469f180351da52873b10317d48b2db3115473ac2
WavyPeng <w1a9v9y3@gmail.com>
2018-07-05 23:44:15 +0800

    插入元素

--- src/wavy/util/dsa/BinarySearchTree.java
+++ src/wavy/util/dsa/BinarySearchTree.java
@@ -1,7 +1,7 @@
 package wavy.util.dsa;
 
 /**
- * 二分搜索树
+ * 二分搜索树（不包含重复元素）
  * Created by WavyPeng on 2018/7/5.
  */
 public class BinarySearchTree<E extends Comparable<E>> {
@@ -44,5 +44,32 @@ public class BinarySearchTree<E extends Comparable<E>> {
      */
     public boolean isEmpty(){
         return size == 0;
+    }
+
+    /**
+     * 插入元素
+     * @param e
+     */
+    public void add(E e){
+        root = add(root,e);
+    }
+
+    /**
+     * 返回插入新节点后二叉搜索树的根
+     * @param node
+     * @param e
+     */
+    private Node add(Node node, E e){
+        if(node == null){
+            size ++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
     }
 }
