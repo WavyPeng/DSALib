dc054f83696f9bfdf5241887b8217f138c088aa6
WavyPeng <w1a9v9y3@gmail.com>
2018-07-30 11:16:15 +0800

    红黑树添加节点操作

--- src/wavy/util/dsa/Tree/RedBlackTree.java
+++ src/wavy/util/dsa/Tree/RedBlackTree.java
@@ -63,6 +63,7 @@ public class RedBlackTree<K extends Comparable<K>,V> {
 
     /**
      * 左旋
+     * 返回旋转后新的根节点
      * @param node
      * @return
      *
@@ -83,6 +84,45 @@ public class RedBlackTree<K extends Comparable<K>,V> {
         node.color = RED;
 
         return x;
+    }
+
+    /**
+     * 右旋
+     * 返回旋转后新的根节点
+     * @param node
+     * @return
+     *
+     *     node               x
+     *    /    \   ---->    /   \
+     *   x     T2          y   node
+     *  / \                   /    \
+     * y  T1                 T1    T2
+     */
+    private Node rightRotate(Node node){
+        Node x = node.left;
+
+        // 右旋
+        node.left = x.right;
+        x.right = node;
+
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+
+    /**
+     * 颜色翻转
+     * 用于向2-3树3节点添加元素的情况
+     *     42  66    42        42(R)
+     *    /   ——>  / \  ——> /  \
+     *   37        37 66     37(B)66(B)
+     * @param node
+     */
+    private void flipColors(Node node){
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
     }
 
     /**
@@ -117,6 +157,65 @@ public class RedBlackTree<K extends Comparable<K>,V> {
         else
             node.value = value;
 
+        // 红黑树性质维护
+        // 左旋
+        if(isRed(node.right) && !isRed(node.left))
+            node = leftRotate(node);
+        // 右旋
+        if(isRed(node.left) && isRed(node.left.left))
+            node = rightRotate(node);
+        // 颜色翻转
+        if(isRed(node.left) && isRed(node.right))
+            flipColors(node);
+
         return node;
+    }
+
+    /**
+     * 返回以node为根节点的红黑树中，key所在的节点
+     * @param node
+     * @param key
+     * @return
+     */
+    private Node getNode(Node node,K key){
+        if(node == null)
+            return null;
+        if(key.equals(node.key))
+            return node;
+        else if(key.compareTo(node.key)<0)
+            return getNode(node.left,key);
+        else
+            return getNode(node.right,key);
+    }
+
+    /**
+     * 红黑树中是否存在某元素
+     * @param key
+     * @return
+     */
+    public boolean contains(K key){
+        return getNode(root,key)!=null;
+    }
+
+    /**
+     * 获取某元素的值
+     * @param key
+     * @return
+     */
+    public V get(K key){
+        Node node = getNode(root,key);
+        return node == null?null:node.value;
+    }
+
+    /**
+     * 设置某节点的值
+     * @param key
+     * @param newValue
+     */
+    public void set(K key,V newValue){
+        Node node = getNode(root,key);
+        if(node == null)
+            throw new IllegalArgumentException(key+" doesn't exist!");
+        node.value = newValue;
     }
 }
